75b2ff897932dacce454dfb340b9584429ba7cd6
Bharath R <78311020+RSBHARATHRS@users.noreply.github.com>
2023-03-20 10:00:52 +0530

    concat script has been added

---
+++ concat-script.js
@@ -0,0 +1,23 @@
+const fs = require('fs-extra');
+const concat = require('concat');
+(async function build() {
+    const projectName = process.argv.slice(2)[0];
+    const version = process.argv.slice(2)[1];
+    const env = process.argv.slice(2)[2] ? process.argv.slice(2)[2] : "";
+    if (!projectName) {
+        console.log('\x1b[31m%s\x1b[0m', 'project name required as argument');
+    } else {
+        const files = [
+            './dist/' + projectName + '/runtime.js',
+            './dist/' + projectName + '/polyfills.js',
+            './dist/' + projectName + '/main.js'        ]
+        await fs.ensureDir('./dist/' + projectName + '/element');
+        if (env) {
+            await concat(files, './dist/' + projectName + '/element/' + env + '-' + projectName + '-' + version + '.js');
+            console.log('\x1b[32m%s\x1b[0m', 'Done generating bundle for ' + env + '-' + projectName + '-' + version);
+        } else {
+            await concat(files, './dist/' + projectName + '/element/' + projectName + '-' + version + '.js');
+            console.log('\x1b[32m%s\x1b[0m', 'Done generating bundle for ' + projectName + '-' + version);
+        }
+    }
+})()

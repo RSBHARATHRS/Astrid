45e8058c91fa66ee3aada14abda1fb7e1c30b26f
Bharath R <78311020+RSBHARATHRS@users.noreply.github.com>
2023-04-01 21:40:02 +0530

    technical almost done

--- src/app/components/recognition-tester/recognition-tester.component.ts
+++ src/app/components/recognition-tester/recognition-tester.component.ts
@@ -1,12 +1,13 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { debounceTime } from 'rxjs/operators';
 import { supportedLang, supportedRegion } from 'src/app/models/supported-lang';
 import { VoiceRecognitionServiceService } from 'src/app/services/voice-recognition-service.service';
 
 interface SettingsForm {
-  lang?: FormControl<string>;
-  subLang?: FormControl<string>;
+  lang: FormControl<string>;
+  subLang: FormControl<string>;
 }
 
 @Component({
@@ -15,17 +16,18 @@ interface SettingsForm {
   styleUrls: ['./recognition-tester.component.scss'],
 })
 
-export class RecognitionTesterComponent {
+export class RecognitionTesterComponent implements OnInit {
 
   speech = '';
   voice = ''
   isVRStarted: boolean = false;
+  isPlayClicked: boolean = false;
   supportedLanguage = supportedLang;
   supportedRegion = supportedRegion;
 
   settingsForm: FormGroup<SettingsForm> = new FormGroup<SettingsForm>({
-    lang: new FormControl('English', { nonNullable: true }),
-    subLang: new FormControl('', { nonNullable: true }),
+    lang: new FormControl('English', { nonNullable: true, validators: [Validators.required] }),
+    subLang: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
   });
 
   constructor(public voiceRecognitionServiceService: VoiceRecognitionServiceService,
@@ -33,9 +35,32 @@ export class RecognitionTesterComponent {
     private cdRef: ChangeDetectorRef) {
   }
 
+  ngOnInit(): void {
+    this.supportedRegion = this.voiceRecognitionServiceService.getLanguageRegionById(Number(JSON.parse(localStorage.getItem("settings")!)?.lang) || 11);
+    if (localStorage.getItem("settings")) {
+      let formData = JSON.parse(localStorage.getItem("settings")!)
+      this.settingsForm.setValue({ lang: formData?.lang, subLang: formData?.subLang });
+    }
+    this.settingsForm.controls.lang.valueChanges.subscribe((value) => {
+      this.supportedRegion = this.voiceRecognitionServiceService.getLanguageRegionById(Number(value));
+      this.settingsForm.controls.subLang.setValue(this.supportedRegion[0]?.code);
+
+    });
+  }
+
   startService() {
+    this.isPlayClicked = true;
+    if(this.settingsForm.invalid)
+      return
     this.isVRStarted = true;
+    this.updateSettings();
     this.recognize();
+  }
+
+  updateSettings() {
+    this.voiceRecognitionServiceService.settings.next(this.settingsForm?.value);
+    let size = localStorage.getItem('settings') ? JSON.parse(localStorage.getItem('settings')!)?.size : '';
+    localStorage.setItem("settings", JSON.stringify({ size: size, ...this.settingsForm.value }));
   }
 
   stopService() {
--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
-import { debounceTime } from 'rxjs/operators';
 import { supportedLang, supportedRegion } from 'src/app/models/supported-lang';
 import { VoiceRecognitionServiceService } from 'src/app/services/voice-recognition-service.service';
 
 interface SettingsForm {
   size: FormControl<number>;
-  lang?: FormControl<number>;
-  subLang?: FormControl<string>;
+  lang: FormControl<string>;
+  subLang: FormControl<string>;
 }
 
 @Component({
@@ -22,11 +21,12 @@ export class SettingsComponent implements OnInit {
 
   supportedLanguage = supportedLang;
   supportedRegion = supportedRegion;
+  isSaveClicked: boolean = false;
 
   settingsForm: FormGroup<SettingsForm> = new FormGroup<SettingsForm>({
     size: new FormControl(50, { nonNullable: true }),
-    lang: new FormControl(11, { nonNullable: true }),
-    subLang: new FormControl('en-IN', { nonNullable: true }),
+    lang: new FormControl('11', { nonNullable: true, validators: [Validators.required] }),
+    subLang: new FormControl('en-IN', { nonNullable: true,  validators: [Validators.required] }),
   });
 
   constructor(public dialogRef: MatDialogRef<SettingsComponent>,
@@ -35,13 +35,14 @@ export class SettingsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.supportedRegion = this.voiceRecognitionServiceService.getLanguageRegionById(JSON.parse(localStorage.getItem("settings")!)?.lang || 11);
+    this.supportedRegion = this.voiceRecognitionServiceService.getLanguageRegionById(Number(JSON.parse(localStorage.getItem("settings")!)?.lang) || 11);
     if (localStorage.getItem("settings")) {
       this.settingsForm.setValue(JSON.parse(localStorage.getItem("settings")!));
     }
-    this.settingsForm.valueChanges.pipe(debounceTime(100)).subscribe((settings) => {
-      this.supportedRegion = this.voiceRecognitionServiceService.getLanguageRegionById(Number(settings?.lang));
-    })
+    this.settingsForm.controls.lang.valueChanges.subscribe((language)=>{
+      this.supportedRegion = this.voiceRecognitionServiceService.getLanguageRegionById(Number(language));
+      this.settingsForm.controls.subLang.setValue(this.supportedRegion[0]?.code);
+    });
   }
 
   reset() {
@@ -49,6 +50,9 @@ export class SettingsComponent implements OnInit {
   }
 
   saveSettings() {
+    this.isSaveClicked = true;
+    if(this.settingsForm.invalid)
+     return;
     this.voiceRecognitionServiceService.settings.next(this.settingsForm?.value);
     localStorage.setItem("settings", JSON.stringify(this.settingsForm.value));
     this.dialogRef.close();
